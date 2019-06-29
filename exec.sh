ecf4defe39a595500fa41e1558026509200cb1a1
Mike Roth <mike.roth@assurant.com>
2019-06-29 16:20:58 -0700

    Update forgot the damn url param

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-/app/Agent/config.sh --unattended --replace --auth atl --username "${TFS_AUTH_USERNAME}" --password "${TFS_AUTH_PASSWORD}" --pool "${TFS_BUILD_POOL}"
+/app/Agent/config.sh --unattended --replace --auth atl --url "${TFS_URL}" --username "${TFS_AUTH_USERNAME}" --password "${TFS_AUTH_PASSWORD}" --pool "${TFS_BUILD_POOL}"
 /app/Agent/run.sh 
